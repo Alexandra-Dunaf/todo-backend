391aac2ba8205e31c00bbbaa814f68326bef39a5
Alexandra_Dunaf <sasha-kalugina@yandex.ru>
2023-09-17 20:35:13 +0400

    add methodes for task

--- src/main/java/ru/dunaf/backend/todo/entity/Task.java
+++ src/main/java/ru/dunaf/backend/todo/entity/Task.java
@@ -3,42 +3,56 @@ package ru.dunaf.backend.todo.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Type;
 
 import java.util.Date;
 
+/*
+
+задачи пользователя
+
+ */
+
 @Entity
 @Table(name = "task", schema = "todolist", catalog = "postgres")
-@Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Cacheable
-@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Task {
 
+    // указываем, что поле заполняется в БД
+    // нужно, когда добавляем новый объект и он возвращается уже с новым id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @EqualsAndHashCode.Include
     private Long id;
 
     private String title;
-    @Basic
-    @Convert(converter = org.hibernate.type.NumericBooleanConverter.class)
-    private Boolean completed;
-    @Column(name = "task_date")
-    private Date date;
 
+    @Basic
+    @Convert(converter = org.hibernate.type.NumericBooleanConverter.class) // для автоматической конвертации числа в true/false
+    private Boolean completed; // 1 = true, 0 = false
+
+    @Column(name = "task_date") // в БД поле называется task_date, т.к. нельзя использовать системное имя date
+    private Date taskDate;
+
+    // задача может иметь только один приоритет (с обратной стороны - один и тот же приоритет может быть использоваться в множестве задач)
     @ManyToOne
-    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    @JoinColumn(name = "priority_id", referencedColumnName = "id") // по каким полям связывать (foreign key)
+    private Priority priority;
+
+    // задача может иметь только одну категорию (с обратной стороны - одна и та же категория может быть использоваться в множестве задач)
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id") // по каким полям связывать (foreign key)
     private Category category;
 
     @ManyToOne
-    @JoinColumn(name = "priority_id", referencedColumnName = "id")
-    private Priority priority;
+    @JoinColumn(name = "user_id", referencedColumnName = "id") // по каким полям связывать (foreign key)
+    private User user; // для какого пользователя задача
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    private User user;
 
     @Override
     public String toString() {
